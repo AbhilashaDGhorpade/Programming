faf47cfb830e126174fcda0af99470dbaf8d4baa
AbhilashaDGhorpade <abhilashadghorpade@gmail.com>
2026-06-16 00:05:46 +0530

    Add files via upload

---
+++ Java-Programming/Practice/program188.java
@@ -0,0 +1,33 @@
+/*
+input = 11
+output = * * * # # # * * * # #(nested loop)
+          
+*/
+import java.util.*;
+class program188
+{
+    public static void Display(int iNo)
+    {
+        int iCnt =0;
+        char ch1 ='\0';
+        char ch2 = '\0';
+
+        for(iCnt=1;iCnt<=iNo;iCnt++)
+        {
+            
+        }
+        
+
+        System.out.println();
+    }
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue = 0;
+
+        System.out.println("Enter the number of elements: ");
+        iValue=sobj.nextInt();
+
+        Display(iValue);
+    }
+}
---
+++ Java-Programming/Practice/program88.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class DigitX
+{
+    public void CountEvenOddDigits(int iNo)
+    {
+        int iDigit = 0;
+        int iCountEven = 0, iCountOdd = 0;
+        while(iNo != 0)
+        {
+            iDigit= iNo % 10;
+            if(iDigit % 2 == 0)
+            {
+                iCountEven++;
+                 
+             
+            }
+            else
+            {
+                iCountOdd++;
+              
+            }
+            iNo= iNo/10;
+        }
+        System.out.println("number of Even Digit:"+iCountEven);
+        System.out.println("number of odd Digit:"+iCountOdd);
+        
+    }
+}
+class program88
+{
+    public static void main(String A[])
+    {
+        Scanner sobj =  new Scanner(System.in);
+        DigitX dobj = new DigitX();
+
+        int iValue = 0;
+        
+
+        System.out.println("Enter number: ");
+        iValue=sobj.nextInt();
+
+        dobj.CountEvenOddDigits(iValue);
+
+     
+    }
+}
