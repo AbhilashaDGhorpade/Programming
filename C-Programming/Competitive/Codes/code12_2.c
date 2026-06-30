84c88a0a51cdb076649917fe1f08e644920c2471
AbhilashaDGhorpade <abhilashadghorpade@gmail.com>
2026-06-30 20:31:24 +0530

    Add files via upload

---
+++ C-Programming/Competitive/Codes/code12_2.c
@@ -0,0 +1,48 @@
+/*
+2.Write a program which accept number from user and check whether it contains 0
+in it or not.
+Input : 2395
+Output : There is no Zero
+Input : 1018
+Output : It Contains Zero
+Input : 9000
+Output : It Contains Zero
+Input : 10687
+Output : It Contains Zero
+*/
+
+#include<stdio.h>
+#define TRUE 1
+#define FALSE 0
+typedef int BOOL;
+BOOL ChkZero(int iNo)
+{
+    int Digit =0;
+    Digit =iNo%2;
+    if(Digit == 0)
+    {
+        return 1;
+    }
+    else
+    {
+     return 0;   
+    }
+
+}
+int main()
+{
+int iValue = 0;
+BOOL bRet = FALSE;
+printf("Enter number: ");
+scanf("%d",&iValue);
+bRet = ChkZero(iValue);++
+if(bRet == TRUE)
+{
+printf("It Contains Zero");
+}
+else
+{
+printf("There is no Zero");
+}
+return 0;
+} 
